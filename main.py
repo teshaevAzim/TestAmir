c85229eb495e957789bd1864d64da87e9e545a45
teshaevAzim <98607345+teshaevAzim@users.noreply.github.com>
2022-04-02 16:20:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,16 +17,6 @@ dispatcher: Dispatcher = updater.dispatcher
     "Mist": "Tuman \U0001F32B"
 }
 
-code_smiles_ru = {
-    "Clear": f"{translater.translate('Toza', 'ru').text} \U00002600",
-    "Clouds": f"{translater.translate('Bulutli', 'ru').text} \U00002601",
-    "Rain": f"{translater.translate('Yomg`ir', 'ru').text} \U00002614",
-    "Drizzle": f"{translater.translate('Yomg`ir', 'ru').text} \U00002614",
-    "Thunderstorm": f"{translater.translate('Chaqmoq', 'ru').text} \U000026A1",
-    "Snow": f"{translater.translate('Qor', 'ru').text} \U0001F328",
-    "Mist": f"{translater.translate('Tuman', 'ru').text} \U0001F32B"
-}
-
 API_KEY = "aeff74af7fec2b68fb196a72a6e458f1"
 
 keyboard = [
