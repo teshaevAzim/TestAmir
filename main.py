76eac3ae8cc929d0a5f6e38dd5789cb0e010252a
teshaevAzim <98607345+teshaevAzim@users.noreply.github.com>
2022-04-02 16:17:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ from telegram.update import Update
 
 updater = Updater(token="5245421544:AAH8KJL3QaJxSSEfuBU1ysI0W3HpfHa5J0c")
 dispatcher: Dispatcher = updater.dispatcher
+    
+ code_smiles_uz = {
+    "Clear": "Toza \U00002600",
+    "Clouds": "Bulutli \U00002601",
+    "Rain": "Yomg`ir \U00002614",
+    "Drizzle": "Yomg`ir \U00002614",
+    "Thunderstorm": "Chaqmoq \U000026A1",
+    "Snow": "Qor \U0001F328",
+    "Mist": "Tuman \U0001F32B"
+}
+
+code_smiles_ru = {
+    "Clear": f"{translater.translate('Toza', 'ru').text} \U00002600",
+    "Clouds": f"{translater.translate('Bulutli', 'ru').text} \U00002601",
+    "Rain": f"{translater.translate('Yomg`ir', 'ru').text} \U00002614",
+    "Drizzle": f"{translater.translate('Yomg`ir', 'ru').text} \U00002614",
+    "Thunderstorm": f"{translater.translate('Chaqmoq', 'ru').text} \U000026A1",
+    "Snow": f"{translater.translate('Qor', 'ru').text} \U0001F328",
+    "Mist": f"{translater.translate('Tuman', 'ru').text} \U0001F32B"
+}
+
+API_KEY = "aeff74af7fec2b68fb196a72a6e458f1"
 
 keyboard = [
     [InlineKeyboardButton("Toshkent", callback_data="toshkent"),
@@ -42,7 +64,7 @@ def callback_handler(update: Update, context: CallbackContext):
     if query.data != "back":
         try:
             City = query.data
-            url = f'http://api.openweathermap.org/data/2.5/weather?q={City}&appid={settings.API_KEY}&units=metric'
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={City}&appid={API_KEY}&units=metric'
             res = requests.get(url)
             data = res.json()
             city = data['name']
